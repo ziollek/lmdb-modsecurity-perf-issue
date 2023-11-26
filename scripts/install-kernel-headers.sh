22334cd882bb730c00de87caf267cf65d77c59ab
Jan Taczanowski <jan.taczanowski@gmail.com>
2023-11-27 00:09:37 +0100

    Fix for compiling kernel headers for openSUSE

--- scripts/install-kernel-headers.sh
+++ scripts/install-kernel-headers.sh
@@ -5,6 +5,7 @@ KERNEL_VERSION=$(uname -r  | cut -d '-' -f 1)
 cd /tmp/linux-${KERNEL_VERSION}
 
 zcat /proc/1/root/proc/config.gz > .config
+sed -ri '/CONFIG_MODULE_SIG_KEY/s/=.+/=""/g' .config
 make all
 make modules_prepare
 make headers_install
