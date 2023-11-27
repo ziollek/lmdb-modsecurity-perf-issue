08725081ec2ce341d6adda84efc491900120e384
Jan Taczanowski <jan.taczanowski@allegro.pl>
2023-11-27 01:28:35 +0100

    Configurable number of CPU cores to use by make

--- scripts/install-kernel-headers.sh
+++ scripts/install-kernel-headers.sh
@@ -5,7 +5,7 @@ KERNEL_VERSION=$(uname -r  | cut -d '-' -f 1)
 cd /tmp/linux-${KERNEL_VERSION}
 
 zcat /proc/1/root/proc/config.gz > .config
-make all
-make modules_prepare
-make headers_install
-make modules_install
+make all -j$1
+make modules_prepare -j$1
+make headers_install -j$1
+make modules_install -j$1
